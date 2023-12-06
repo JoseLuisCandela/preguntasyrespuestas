08d1e680ec146b49fe9295f63f2919f5bad13c39
JoseLuisCandela <jlcandela@itpfp.com>
2023-12-06 19:16:31 +0100

    prueba 1 github

---
+++ src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String preguntas[] = {
+        
+            "¿Cuanto es la mitad de dos mas dos?",
+            "edificio empieza por e y termina por...",
+            "¿cual seria el siguiente numero de la secuencia?0,1,1,2,3,5,8,13,...",
+            "¿cual es el apellido de bob esponja?",
+            "¿que mes tiene 28 dias?"
+
+        };
+        String respuestas[]={
+
+            "3",
+           "t",
+            "21",
+            "pantalones cuadrados",
+            "todos"
+        }   ;
+    }
+}
