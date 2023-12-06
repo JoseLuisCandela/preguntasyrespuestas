3722354fbee0f04555bb00f460da3fe31f68993a
AlbuSama <alosada@itpfp.com>
2023-12-06 19:48:59 +0100

    Segunda prueba

---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,28 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        
+                String preguntas[] = {
+        
+                    "Oro parece y plata no es, quien no lo adivine tonto es",
+                    "Dos abanicos que no paran en todo el día, pero cuando duermes se quedan quietos",
+                    "¿Llevo mi casa a cuestas y camino sin tener patas",
+                    "Pese a tener 4 patas, no puedo correr ni caminar. ¿Qué soy?",
+                    "¿Lana sube y lana baja. ¿De qué se trata?",
+                    
+        
+                }
+                String respuestas[]={
+        
+                    "Platano",
+                   "Las pestañas",
+                    " El caracol",
+                    "Silla",
+                    "Navaja",
+                }   ;
+        
+        
+            }
+        }
+    
